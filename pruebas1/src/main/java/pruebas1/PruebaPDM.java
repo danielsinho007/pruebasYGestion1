a9539fb154b20f8008cb0fe63239029edf92c1d8
Daniel Montoya Cardenas <dmontoyac@ias.com.co>
2019-10-26 17:01:00 -0500

    Se crea clase de prueba

---
+++ pruebas1/src/main/java/pruebas1/PruebaPDM.java
@@ -0,0 +1,12 @@
+package pruebas1;
+
+public class PruebaPDM {
+
+	int Xx;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo");
+
+	}
+
+}
